0d7f5dc6c503a9bab61f9069dc93e940363e9202
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-06-30 17:30:21 -0700

    Fixed bugs for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - This is the entry point for our code
+ * main - This is the entry point for our code.
  *
- * Description - Prints out the string and exits
- * Return - Always 0 (Success).
+ * Return: Always returns 0 (Success).
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
