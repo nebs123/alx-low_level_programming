5c00f2deb349cf95a6e466f58a4fd72212b88e18
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-07-05 19:09:24 -0700

    tentative 104-fiboacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the first 98 fibonnaci numbers
+ *
+ * Return: return 0(success)
+ */
+int main(void)
+{
+	unsigned long first, second, counter;
+
+	first = 1;
+	second = 2;
+
+	printf("%lu, %lu", first, second);
+
+	for (counter = 2; counter < 98; counter++)
+	{
+		unsigned long new = first + second;
+
+		first = second;
+		second = new;
+		printf(", %lu", new);
+	}
+	printf("\n");
+	return (0);
+}
