ff50ec8af7ff966c913fa035f80e9cb7849095cd
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-07-07 10:23:11 -0700

    changed only 1 line from original

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests the function positive_or_negative
- *
- * Return: returns 0 (success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
 
-	positive_or_negative(0);
 	return (0);
-
-
 }
