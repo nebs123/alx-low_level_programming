1ae026c34557bc69de03ac68fa9bba88093b9c2b
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-07-07 16:01:29 -0700

    fixed bugs in 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,25 @@
 #include "main.h"
 
 /**
- * printDigits - prints out the number n
+ * print_number - prints out the number n
  * @n: the number to printed out
  *
  * Return: nothing (void)
  */
 void print_number(int n)
 {
-	if (n == 0 || n / 10 == 0)
+	if (n == 0)
+	{
 		_putchar(n + '0');
+	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		print_number(-n);
+	}
+	else if (n / 10 == 0)
+	{
+		_putchar(n + '0');
 	}
 	else
 	{
