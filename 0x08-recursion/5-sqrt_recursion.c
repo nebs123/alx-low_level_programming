08ff2a2202c9bc49cf1bd9c0ed733e95a1201553
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-07-20 15:32:33 -0700

    bug fixed for 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,25 +10,22 @@ int min_square(int n, int k);
  */
 int _sqrt_recursion(int n)
 {
-	int next_sq = min_square(n, n);
-
-	if (next_sq * next_sq == n)
-		return (next_sq);
-	else
-		return (-1);
+	return (min_square(n, 1));
 }
 
 /**
- * min_square - return the root of the largest square no less or equal to n
+ * min_square - checks for the square root of n beginning from base k
  * @n: the number to find the square of
  * @k: the no whose square is to be checked
  *
- * Return: square root of the least natural square less than n
+ * Return: square root of n if it exists other wise -1
  */
 int min_square(int n, int k)
 {
-	if (k * k <= n)
+	if (k * k == n)
 		return (k);
+	else if (k * k > n)
+		return (-1)
 	else
-		return (min_square(n, k - 1));
+		return (min_square(n, k + 1));
 }
