5635ed03cbad9acb084f86c60947c2165feea377
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-07-26 14:47:16 -0700

    0x0B-malloc_free: done with advanced task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * count_words - counts the number of words in string
+ * @ptr: input string
+ *
+ * Return: number of space separated words in strings
+ */
+int count_words(char *ptr)
+{
+	int count = 0;
+
+	while (*ptr)
+	{
+		while (*ptr && *ptr == ' ')
+			ptr++;
+		if (*ptr)
+			count++;
+		while (*ptr && *ptr != ' ')
+			ptr++;
+	}
+
+	return (count);
+}
+
+/**
+ * strtow - returns an array of words found in str
+ * @str: string input
+ *
+ * Return: an array of strings which are words in str, NULL on failure
+ */
+char **strtow(char *str)
+{
+	int n, words, wordlen,z;
+	char **wordArr, *str_count, *word;
+
+	if (str == NULL || !*str)
+		return (NULL);
+	words = count_words(str);
+	wordArr = (char **) malloc(sizeof(int *) * (words + 1));
+	if (wordArr == NULL)
+		return (NULL);
+	for (n = 0; n < words; n++)
+	{
+		while (*str && *str == ' ')
+			str++;
+		str_count = str;
+		wordlen = 0;
+		while (*str_count && *str_count != ' ')
+		{
+			wordlen++;
+			str_count++;
+		}
+		word = (char *) malloc(sizeof(char) * (wordlen + 1));
+		if (word == NULL)
+		{
+			for (z = 0; z < n; z++)
+			{
+				free(*(wordArr + z));
+			}
+			free(wordArr);
+			return (NULL);
+		}
+		*(wordArr + n) = word;
+		while (*str && *str != ' ')
+		{
+			*word++ = *str++;
+		}
+		*word = '\0';
+	}
+	*(wordArr + n) = NULL;
+	return (wordArr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -5,3 +5,4 @@
 3-alloc_grid.c - contains function that allocates memory for a two dimensional array and initializes it to 0
 4-free_grid.c - contains function that deallocate the memory allocated by alloc_grid() function
 100-argstostr.c - contains function that concatenates all the arguements of command line
+101-strtow.c - contains function that splits string into an array of string made up of the words from input string
