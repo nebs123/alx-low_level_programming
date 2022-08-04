d45371c5ddafea60ef4dfa578145d307ea1e2d21
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-08-04 15:02:48 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,37 +10,39 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	unsigned int x = 0;
+	unsigned int x = 0, sep = 0;
 	char *str;
 
 	va_start(list, format);
 	while (*(format + x))
 	{
-		if (x != 0 &&
-		    (*(format + x) == 'c' ||  *(format + x) == 's'
+		if (sep && (*(format + x) == 'c' ||  *(format + x) == 's'
 		     || *(format + x) == 'f' || *(format + x) == 'i'))
 			printf(", ");
-
 		switch (*(format + x))
 		{
 		case 'c':
 			printf("%c", (char) va_arg(list, int));
+			sep = 1;
 			break;
 		case 'i':
 			printf("%d", va_arg(list, int));
+			sep = 1;
 			break;
 		case 'f':
 			printf("%f", va_arg(list, double));
+			sep = 1;
 			break;
 		case 's':
 			str = va_arg(list, char *);
 			if (str)
 				printf("%s", str);
-			while(!str)
+			while (!str)
 			{
 				printf("(nil)");
-				str = "1";
+				break;
 			}
+			sep = 1;
 			break;
 		}
 		x++;
